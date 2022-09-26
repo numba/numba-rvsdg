a1af1b1cee614c981b2eaac89fb7d9e29254bba9
esc <esc@users.noreply.github.com>
2022-09-26 15:56:11 +0200

    cleanup test_simulate
    As title

--- test_simulate.py
+++ test_simulate.py
@@ -2,6 +2,7 @@ from byteflow2 import ByteFlow, ByteFlowRenderer
 from simulator import Simulator
 from pprint import pprint
 
+
 def foo(x):
     c = 0
     for i in range(x):
@@ -14,18 +15,18 @@ def foo(x):
 def test_foo():
     flow = ByteFlow.from_bytecode(foo)
     pprint(flow.bbmap)
-    flow = flow._join_returns()._restructure_loop()
+    flow = flow.restructure()
     pprint(flow.bbmap)
     # pprint(rtsflow.bbmap)
     ByteFlowRenderer().render_byteflow(flow).view()
+
     sim = Simulator(flow, foo.__globals__)
     ret = sim.run(dict(x=0))
-    #breakpoint()
     assert ret == foo(x=0)
 
-    # sim = Simulator(rtsflow, foo.__globals__)
-    # ret = sim.run(dict(x=3))
-    # assert ret == foo(x=3)
+    sim = Simulator(flow, foo.__globals__)
+    ret = sim.run(dict(x=100))
+    assert ret == foo(x=100)
 
 if __name__ == "__main__":
     test_foo()
