e4b5eaa2ecb219cbe837b83dc83022d32c8ed1d1
kc611 <ckaustubhm06@gmail.com>
2023-06-19 11:21:41 +0530

    Refactored basicblock doumentation to numpy format

--- numba_rvsdg/core/datastructures/basic_block.py
+++ numba_rvsdg/core/datastructures/basic_block.py
@@ -8,36 +8,67 @@ from numba_rvsdg.core.utils import _next_inst_offset
 
 @dataclass(frozen=True)
 class BasicBlock:
-    """
-        The BasicBlock class represents an atomic basic block in a control flow graph.
-        Note that the BasicBlock class is defined with the `frozen=True`` parameter,
-        making instances of this class immutable.
+    """Basic building block of an SCFG graph.
+
+    The BasicBlock class represents an atomic basic block in a control flow graph.
+
+    Attributes
+    ----------
+    name: str
+        The corresponding name for this block.
+
+    _jump_targets: Tuple[str]
+        Jump targets (branch destinations) for this block
+
+    backedges: Tuple[str]
+        Backedges for this block.
     """
 
     name: str
-    """The corresponding name for this block.  """
 
     _jump_targets: Tuple[str] = tuple()
-    """Jump targets (branch destinations) for this block"""
 
     backedges: Tuple[str] = tuple()
-    """Backedges for this block."""
 
     @property
     def is_exiting(self) -> bool:
-        """Indicates whether this block is an exiting block, i.e., 
-        it does not have any jump targets."""
+        """Indicates whether this block is an exiting block, i.e.,
+        it does not have any jump targets.
+
+        Returns
+        -------
+        is_exiting: bool
+            True if the current block is an exiting block, False if it
+            isn't
+
+        """
         return not self.jump_targets
 
     @property
     def fallthrough(self) -> bool:
-        """Indicates whether this block has a single fallthrough jump target."""
+        """Indicates whether this block has a single fallthrough jump target.
+
+        Returns
+        -------
+        is_exiting: bool
+            True if the current block is a fallthorough block, False if it
+            isn't
+
+        """
         return len(self._jump_targets) == 1
 
     @property
     def jump_targets(self) -> Tuple[str]:
-        """Retrieves the jump targets for this block, 
-        excluding any jump targets that are also backedges."""
+        """Retrieves the jump targets for this block,
+        excluding any jump targets that are also backedges.
+
+        Returns
+        -------
+        jump_targets: Tuple[str]
+            Tuple of jump targets of this block, (exludes backedges).
+            Ordered according to their position.
+
+        """
         acc = []
         for j in self._jump_targets:
             if j not in self.backedges:
@@ -46,6 +77,17 @@ class BasicBlock:
 
     def declare_backedge(self, target: str) -> "BasicBlock":
         """Declare one of the jump targets as a backedge of this block.
+
+        Parameters
+        ----------
+        target: str
+            The jump target that is to be declared as a backedge.
+
+        Returns
+        -------
+        basic_block: BasicBlock
+            The resulting block
+
         """
         if target in self.jump_targets:
             assert not self.backedges
@@ -54,46 +96,90 @@ class BasicBlock:
 
     def replace_jump_targets(self, jump_targets: Tuple) -> "BasicBlock":
         """Replaces jump targets of this block by the given tuple.
+
+        This method replaces the jump targets of  the current BasicBlock.
         The provided jump targets must be in the same order as their
         intended original replacements.
 
         Note that replacing jump targets will not replace the backedge
         tuple, so replacement for any jump targets that is declared as
-        a backedge needs to be updated separately using replace_backedges"""
+        a backedge needs to be updated separately using replace_backedges
+
+        Parameters
+        ----------
+        jump_targets: Tuple
+            The new jump target tuple. Must be ordered.
+
+        Returns
+        -------
+        result: BasicBlock
+            The resulting BasicBlock
+
+        """
         return replace(self, _jump_targets=jump_targets)
 
     def replace_backedges(self, backedges: Tuple) -> "BasicBlock":
-        """Replace back edges of this block by the given tuple.
+        """Replaces back edges of this block by the given tuple.
+
+        This method replaces the back edges of  the current BasicBlock.
         The provided back edges must be in the same order as their
-        intended original replacements."""
+        intended original replacements.
+
+        Parameters
+        ----------
+        backedges: Tuple
+            The new back edges tuple. Must be ordered.
+
+        Returns
+        -------
+        result: BasicBlock
+            The resulting BasicBlock
+
+        """
         return replace(self, backedges=backedges)
 
 
 @dataclass(frozen=True)
 class PythonBytecodeBlock(BasicBlock):
+    """The PythonBytecodeBlock class is a subclass of the BasicBlock that
+    represents basic blocks with Python bytecode.
+
+    Attributes
+    ----------
+    begin: int
+        The starting bytecode offset.
+
+    end: int
+        The bytecode offset immediate after the last bytecode of the block.
     """
-        The PythonBytecodeBlock class is a subclass of the BasicBlock class
-        and represents a basic block of Python bytecode. It inherits all
-        attributes and methods from the BasicBlock class as well as it's
-        immutability properties.
-   """
 
     begin: int = None
-    """The starting bytecode offset."""
 
     end: int = None
-    """The bytecode offset immediate after the last bytecode of the block."""
 
     def get_instructions(
         self, bcmap: Dict[int, dis.Instruction]
     ) -> List[dis.Instruction]:
-        """
-            Retrieves a list of dis.Instruction objects corresponding to
-            the instructions within the bytecode block. The bcmap parameter
-            is a dictionary mapping bytecode offsets to dis.Instruction
-            objects. This method iterates over the bytecode offsets within
-            the begin and end range, retrieves the corresponding dis.Instruction
-            objects from bcmap, and returns a list of these instructions.
+        """Retrieves a list of dis.Instruction objects corresponding to
+        the instructions within the bytecode block.
+
+        In this method, The bcmap parameter is a dictionary mapping bytecode
+        offsets to dis.Instruction objects. This method iterates over the
+        bytecode offsets within the begin and end range, retrieves the
+        corresponding dis.Instruction objects from bcmap, and returns a list
+        of these instructions.
+
+        Parameters
+        ----------
+        bcmap: Dict[int, dis.Instruction]
+            Dictionary mapping bytecode offsets to dis.Instruction objects
+
+        Return
+        ------
+        out: List[dis.Instruction]
+            The requested instructions according to bcmap between begin and
+            end offsets
+
         """
         begin = self.begin
         end = self.end
@@ -113,90 +199,97 @@ class PythonBytecodeBlock(BasicBlock):
 
 @dataclass(frozen=True)
 class SyntheticBlock(BasicBlock):
-    """
-        The SyntheticBlock class is a subclass of the BasicBlock class and
-        represents a artificially added block in a control flow graph. It serves as
-        a base class for other artificially added block types. This class inherits
-        all attributes and methods from the BasicBlock class.
+    """The SyntheticBlock represents a artificially added block in a
+    structured control flow graph (SCFG).
     """
     pass
 
 @dataclass(frozen=True)
 class SyntheticExit(SyntheticBlock):
-    """
-        The SyntheticExit class is a subclass of the SyntheticBlock class
-        and represents a artificially added exit block in a control flow graph. 
-        It is used to denote an exit point in the data flow. This class
-        inherits all attributes and methods from the SyntheticBlock and 
-        BasicBlock classes.
+    """The SyntheticExit class represents a artificially added exit block
+    in a structured control flow graph (SCFG).
     """
     pass
 
 @dataclass(frozen=True)
 class SyntheticReturn(SyntheticBlock):
-    """
-        The SyntheticReturn class is a subclass of the SyntheticBlock class 
-        and represents a artificially added return block in a data flow graph. It 
-        is used to denote a return point in the data flow. This class 
-        inherits all attributes and methods from the SyntheticBlock and 
-        BasicBlock classes.
+    """The SyntheticReturn class represents a artificially added return block
+    in a structured control flow graph (SCFG).
     """
     pass
 
 @dataclass(frozen=True)
 class SyntheticTail(SyntheticBlock):
-    """
-        The SyntheticTail class is a subclass of the SyntheticBlock class and 
-        represents a artificially added tail block in a control flow graph. It is used 
-        to denote a tail call point in the data flow. This class inherits 
-        all attributes and methods from the SyntheticBlock and BasicBlock 
-        classes.
+    """The SyntheticTail class represents a artificially added tail block
+    in a structured control flow graph (SCFG).
     """
     pass
 
 @dataclass(frozen=True)
 class SyntheticFill(SyntheticBlock):
+    """The SyntheticFill class represents a artificially added fill block
+    in a structured control flow graph (SCFG).
     """
-        The SyntheticFill class is a subclass of the SyntheticBlock class 
-        and represents a artificially added fill block in a control flow graph. It is 
-        used to denote a fill point in the data flow. This class inherits 
-        all attributes and methods from the SyntheticBlock and BasicBlock 
-        classes.
-   """
     pass
 
 @dataclass(frozen=True)
 class SyntheticAssignment(SyntheticBlock):
-    """
-        The SyntheticAssignment class is a subclass of the SyntheticBlock
-        class and represents a artificially added assignment block in a control
-        flow graph. It is used to denote a block where variable assignments
-        occur. This class inherits all attributes and methods from the
-        SyntheticBlock and BasicBlock classes.
-    """
-    variable_assignment: dict = None
-    """
-        A dictionary representing the variable assignments
-        that occur within the artificially added assignment block. It maps
+    """The SyntheticFill class represents a artificially added assignment block
+    in a structured control flow graph (SCFG).
+
+    This block is responsible for giving variables their values,
+    once the respective block is executed.
+
+    Attributes
+    ----------
+    variable_assignment: dict
+        A dictionary representing the variable assignments. It maps
         the variable name to the value that is is assigned when
         the block is executed.
     """
+    variable_assignment: dict = None
 
 
 @dataclass(frozen=True)
 class SyntheticBranch(SyntheticBlock):
+    """The SyntheticBranch class represents a artificially added branch block
+    in a structured control flow graph (SCFG).
+
+    Attributes
+    ----------
+    variable: str
+        The variable on the basis of which branching will happen when the
+        current block is executed.
+    branch_value_table: dict
+        The value table maps variable values to the repective jump target
+        to be executed on the basis of that value.
+    """
     variable: str = None
     branch_value_table: dict = None
 
     def replace_jump_targets(self, jump_targets: Tuple) -> "BasicBlock":
         """Replaces jump targets of this block by the given tuple.
+
+        This method replaces the jump targets of  the current BasicBlock.
         The provided jump targets must be in the same order as their
         intended original replacements. Additionally also updates the
         branch value table of the branch block.
 
         Note that replacing jump targets will not replace the backedge
         tuple, so replacement for any jump targets that is declared as
-        a backedge needs to be updated separately using replace_backedges"""
+        a backedge needs to be updated separately using replace_backedges
+
+        Parameters
+        ----------
+        jump_targets: Tuple
+            The new jump target tuple. Must be ordered.
+
+        Returns
+        -------
+        result: BasicBlock
+            The resulting BasicBlock
+
+        """
 
         old_branch_value_table = self.branch_value_table
         new_branch_value_table = {}
@@ -224,66 +317,71 @@ class SyntheticBranch(SyntheticBlock):
 
 @dataclass(frozen=True)
 class SyntheticHead(SyntheticBranch):
-    """
-        The SyntheticHead class is a subclass of the SyntheticBranch 
-        class and represents a artificially added head block in a control flow 
-        graph. It is used to denote the head of a artificially added branch. 
-        This class inherits all attributes and methods from the 
-        SyntheticBranch and BasicBlock classes.
+    """The SyntheticHead class represents a artificially added head block
+    in a structured control flow graph (SCFG).
     """
     pass
 
 
 @dataclass(frozen=True)
 class SyntheticExitingLatch(SyntheticBranch):
-    """
-        The SyntheticExitingLatch class is a subclass of the SyntheticBranch 
-        class and represents a artificially added exiting latch block in a control 
-        flow graph. It is used to denote a artificially added latch block that is 
-        also an exit point in the data flow. This class inherits all 
-        attributes and methods from the SyntheticBranch and BasicBlock 
-        classes.
+    """The SyntheticExitingLatch class represents a artificially added exiting latch
+    block in a structured control flow graph (SCFG).
     """
     pass
 
 
 @dataclass(frozen=True)
 class SyntheticExitBranch(SyntheticBranch):
-    """
-        The SyntheticExitBranch class is a subclass of the SyntheticBranch 
-        class and represents a synthetic exit branch block in a control flow 
-        graph. It is used to denote a synthetic branch block that leads to 
-        an exit point in the control flow. This class inherits all attributes 
-        and methods from the SyntheticBranch and BasicBlock classes.
+    """The SyntheticExitBranch class represents a artificially added exit branch
+    block in a structured control flow graph (SCFG).
     """
     pass
 
 
 @dataclass(frozen=True)
 class RegionBlock(BasicBlock):
+    """The RegionBlock is a BasicBlock that represents a region in a
+    structured control flow graph (SCFG) object.
+
+    Attributes
+    ----------
+    kind: str
+        The kind of region. Can be 'head', 'tail', 'branch',
+        'loop' or 'meta' strings
+    parent_region: "RegionBlock"
+        The parent region of this region as per the SCFG.
+    header: str
+        The header node of the region
+    subregion: "SCFG"
+        The subgraph as an independent SCFG. Note that in case
+        of subregions the exiting node may point to blocks outside
+        of the current SCFG object.
+    exiting: str
+        The exiting node of the region.
     """
-        The RegionBlock class is a subclass of the BasicBlock class and 
-        represents a block within a region in a control flow graph. It 
-        extends the BasicBlock class with additional attributes and methods. 
-   """
+
     kind: str = None
-    """The kind of region. Can be 'head', 'tail', 'branch',
-    'loop' or 'meta' strings"""
     parent_region: "RegionBlock" = None
-    """The parent region of this region as per the SCFG."""
     header: str = None
-    """The header node of the region"""
     subregion: "SCFG" = None
-    """The subgraph as an independent SCFG. Note that in case
-    of subregions the exiting node may point to blocks outside
-    of the current SCFG object."""
     exiting: str = None
-    """The exiting node of the region."""
 
     def replace_header(self, new_header):
-        """Does inplace replacement of the header block."""
+        """This method performs a inplace replacement of the header block.
+
+        Parameters
+        ----------
+        new_header: str
+            The new header block of the region represented by the RegionBlock.
+        """
         object.__setattr__(self, "header", new_header)
 
     def replace_exiting(self, new_exiting):
-        """Does, inplace replacement of the exiting block."""
+        """This method performs a inplace replacement of the header block.
+
+        Parameters
+        ----------
+        new_exiting: str
+            The new exiting block of the region represented by the RegionBlock."""
         object.__setattr__(self, "exiting", new_exiting)
