a54e1d36133c68f7d0c564d46576949e0a319139
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2023-06-21 16:09:54 -0500

    Fix formatting

--- numba_rvsdg/tests/test_mock_asm.py
+++ numba_rvsdg/tests/test_mock_asm.py
@@ -1,8 +1,5 @@
 from dataclasses import dataclass, field
-from enum import IntEnum
-from pprint import pprint
 from io import StringIO
-from typing import IO, Dict
 import random
 import textwrap
 import os
@@ -10,10 +7,27 @@ import os
 from mock_asm import ProgramGen, parse, VM, Inst, GotoOperands, BrCtrOperands
 
 
+from numba_rvsdg.rendering.rendering import SCFGRenderer
+
+
+from numba_rvsdg.core.datastructures.scfg import SCFG, NameGenerator
+from numba_rvsdg.core.datastructures.basic_block import (
+    BasicBlock,
+    RegionBlock,
+    SyntheticBlock,
+)
+from numba_rvsdg.core.transformations import (
+    restructure_loop,
+    restructure_branch,
+)
+
+
 DEBUGGRAPH = int(os.environ.get("DEBUGGRAPH", 0))
 
+
 def test_mock_asm():
-    asm = textwrap.dedent("""
+    asm = textwrap.dedent(
+        """
             print Start
             goto A
         label A
@@ -22,7 +36,8 @@ def test_mock_asm():
             brctr A B
         label B
             print B
-    """)
+    """
+    )
 
     instlist = parse(asm)
     assert instlist[0].operands.text == "Start"
@@ -42,7 +57,8 @@ def test_mock_asm():
 
 
 def test_double_exchange_loop():
-    asm = textwrap.dedent("""
+    asm = textwrap.dedent(
+        """
             print Start
        label A
             print A
@@ -54,7 +70,8 @@ def test_double_exchange_loop():
             brctr A Exit
         label Exit
             print Exit
-    """)
+    """
+    )
     instlist = parse(asm)
     with StringIO() as buf:
         VM(buf).run(instlist)
@@ -73,11 +90,9 @@ def test_program_gen():
         print(str(i).center(80, "="))
         asm = pg.generate_program()
 
-
         instlist = parse(asm)
         with StringIO() as buf:
-            terminated = VM(buf).run(instlist,
-                                     max_step=1000)
+            terminated = VM(buf).run(instlist, max_step=1000)
             got = buf.getvalue().split()
             if terminated:
                 print(asm)
@@ -85,16 +100,6 @@ def test_program_gen():
                 ct_term += 1
     print("terminated", ct_term, "total", total)
 
-from numba_rvsdg.core.datastructures.scfg import SCFG, NameGenerator
-from numba_rvsdg.core.datastructures.basic_block import (
-    BasicBlock,
-    RegionBlock,
-    SyntheticBlock,
-)
-from numba_rvsdg.core.transformations import (
-    restructure_loop,
-    restructure_branch,
-)
 
 @dataclass(frozen=True)
 class MockAsmBasicBlock(BasicBlock):
@@ -109,10 +114,12 @@ def _iter_subregions(scfg: "SCFG"):
             yield node
             yield from _iter_subregions(node.subregion)
 
+
 def recursive_restructure_loop(scfg: "SCFG"):
     restructure_loop(scfg.region)
     for region in _iter_subregions(scfg):
         restructure_loop(region)
+
 
 def recursive_restructure_branch(scfg: "SCFG"):
     restructure_branch(scfg.region)
@@ -134,9 +141,8 @@ def to_scfg(instlist: list[Inst]) -> SCFG:
                 labels.add(pc + 1)
     block_map_graph = {}
     scfg = SCFG(block_map_graph, NameGenerator())
-    bb_offsets =sorted(labels)
+    bb_offsets = sorted(labels)
     labelmap = {}
-
 
     for begin, end in zip(bb_offsets, bb_offsets[1:]):
         labelmap[begin] = label = scfg.name_gen.new_block_name("mock")
@@ -147,8 +153,7 @@ def to_scfg(instlist: list[Inst]) -> SCFG:
         if isinstance(inst.operands, GotoOperands):
             targets = [inst.operands.jump_target]
         elif isinstance(inst.operands, BrCtrOperands):
-            targets = [inst.operands.true_target,
-                       inst.operands.false_target]
+            targets = [inst.operands.true_target, inst.operands.false_target]
         elif end < len(instlist):
             targets = [end]
         else:
@@ -182,17 +187,16 @@ def to_scfg(instlist: list[Inst]) -> SCFG:
 
     scfg.join_returns()
     if DEBUGGRAPH:
-        MockAsmRenderer(scfg).view('jointed')
+        MockAsmRenderer(scfg).view("jointed")
     recursive_restructure_loop(scfg)
     if DEBUGGRAPH:
-        MockAsmRenderer(scfg).view('loop')
+        MockAsmRenderer(scfg).view("loop")
     recursive_restructure_branch(scfg)
     if DEBUGGRAPH:
-        MockAsmRenderer(scfg).view('branch')
+        MockAsmRenderer(scfg).view("branch")
     return scfg
 
 
-from numba_rvsdg.rendering.rendering import SCFGRenderer
 class MockAsmRenderer(SCFGRenderer):
     def render_block(self, digraph, name: str, block: BasicBlock):
         if isinstance(block, MockAsmBasicBlock):
@@ -203,11 +207,17 @@ class MockAsmRenderer(SCFGRenderer):
             for inst in block.bbinstlist:
                 instbody.append(f"\l    {inst}")
 
-            body = name + "\l"+ \
-                    "\n" + "".join(instbody) + \
-                    "\n" + \
-                    "\njump targets: " + str(block.jump_targets) + \
-                    "\nback edges: " + str(block.backedges)
+            body = (
+                name
+                + "\l"
+                + "\n"
+                + "".join(instbody)
+                + "\n"
+                + "\njump targets: "
+                + str(block.jump_targets)
+                + "\nback edges: "
+                + str(block.backedges)
+            )
 
             digraph.node(str(name), shape="rect", label=body)
         else:
@@ -239,7 +249,7 @@ class Simulator:
                 else:
                     break  # break and return action
             else:
-                assert False, "unreachable" # in case of coding errors
+                assert False, "unreachable"  # in case of coding errors
 
     def run_block(self, block):
         print("run block", block.name)
@@ -274,7 +284,7 @@ class Simulator:
                 else:
                     break  # break and return action
             else:
-                assert False, "unreachable" # in case of coding errors
+                assert False, "unreachable"  # in case of coding errors
 
         self.region_stack.pop()
         return action
@@ -297,14 +307,16 @@ class Simulator:
         else:
             return {"return": None}
 
-   ### Synthetic Instructions ###
+    ### Synthetic Instructions ###
     def synth_SyntheticAssignment(self, control_label, block):
         self.ctrl_varmap.update(block.variable_assignment)
         [label] = block.jump_targets
         return {"jumpto": label}
 
     def _synth_branch(self, control_label, block):
-        jump_target = block.branch_value_table[self.ctrl_varmap[block.variable]]
+        jump_target = block.branch_value_table[
+            self.ctrl_varmap[block.variable]
+        ]
         return {"jumpto": jump_target}
 
     def synth_SyntheticExitingLatch(self, control_label, block):
@@ -334,6 +346,7 @@ def simulate_scfg(scfg: SCFG):
         Simulator(scfg, buf, max_step=1000).run()
         return buf.getvalue()
 
+
 def compare_simulated_scfg(asm):
     instlist = parse(asm)
     scfg = to_scfg(instlist)
@@ -342,13 +355,14 @@ def compare_simulated_scfg(asm):
         terminated = VM(buf).run(instlist, max_step=1000)
         assert terminated
         expect = buf.getvalue()
-    print("EXPECT".center(80, '='))
+    print("EXPECT".center(80, "="))
     print(expect)
 
     got = simulate_scfg(scfg)
     assert got == expect
 
     return scfg
+
 
 def ensure_contains_region(scfg: SCFG, loop: int, branch: int):
     def recurse_find_regions(bbmap: SCFG):
@@ -361,16 +375,18 @@ def ensure_contains_region(scfg: SCFG, loop: int, branch: int):
     count_loop = 0
     count_branch = 0
     for reg in regions:
-        if reg.kind == 'loop':
+        if reg.kind == "loop":
             count_loop += 1
-        elif reg.kind == 'branch':
+        elif reg.kind == "branch":
             count_branch += 1
 
     assert loop == count_loop
     assert branch == count_branch
 
+
 def test_mock_scfg_loop():
-    asm = textwrap.dedent("""
+    asm = textwrap.dedent(
+        """
             print Start
             goto A
         label A
@@ -379,14 +395,16 @@ def test_mock_scfg_loop():
             brctr A B
         label B
             print B
-    """)
+    """
+    )
     scfg = compare_simulated_scfg(asm)
     ensure_contains_region(scfg, loop=1, branch=0)
 
 
 def test_mock_scfg_head_cycle():
     # Must manually enforce the the entry block only has no predecessor
-    asm = textwrap.dedent("""
+    asm = textwrap.dedent(
+        """
             print Start
             goto S
         label S
@@ -398,12 +416,15 @@ def test_mock_scfg_head_cycle():
             brctr S B
         label B
             print B
-    """)
+    """
+    )
     scfg = compare_simulated_scfg(asm)
     ensure_contains_region(scfg, loop=1, branch=0)
 
+
 def test_mock_scfg_diamond():
-    asm = textwrap.dedent("""
+    asm = textwrap.dedent(
+        """
             print Start
             ctr 1
             brctr A B
@@ -415,13 +436,15 @@ def test_mock_scfg_diamond():
             goto C
         label C
             print C
-    """)
+    """
+    )
     scfg = compare_simulated_scfg(asm)
     ensure_contains_region(scfg, loop=0, branch=2)
 
 
 def test_mock_scfg_double_exchange_loop():
-    asm = textwrap.dedent("""
+    asm = textwrap.dedent(
+        """
             print Start
             goto A
        label A
@@ -434,18 +457,19 @@ def test_mock_scfg_double_exchange_loop():
             brctr A Exit
         label Exit
             print Exit
-    """)
+    """
+    )
     scfg = compare_simulated_scfg(asm)
     # branch count may be more once branch restructuring is fixed
     ensure_contains_region(scfg, loop=1, branch=4)
 
 
-
 def test_mock_scfg_doubly_loop():
-    asm = textwrap.dedent("""
+    asm = textwrap.dedent(
+        """
             print Entry
             goto Head
-       label Head
+        label Head
             print Head
             goto Midloop
         label Midloop
@@ -458,7 +482,8 @@ def test_mock_scfg_doubly_loop():
             brctr Midloop Exit
         label Exit
             print Exit
-    """)
+    """
+    )
     scfg = compare_simulated_scfg(asm)
     # Note: branch number if not correct
     ensure_contains_region(scfg, loop=2, branch=6)
@@ -482,6 +507,7 @@ def run_fuzzer(seed):
             compare_simulated_scfg(asm)
             return True
 
+
 failing_case = {
     0,
     7,
@@ -496,6 +522,7 @@ failing_case = {
     153,
 }
 
+
 def test_mock_scfg_fuzzer(total=1000):
     # tested up to total=100000
     ct_term = 0
@@ -509,7 +536,7 @@ def test_mock_scfg_fuzzer(total=1000):
         except Exception:
             print("Failed case:", i)
         else:
-            print('ok', i)
+            print("ok", i)
     print("terminated", ct_term, "total", total)
 
 
@@ -525,8 +552,10 @@ def test_mock_scfg_fuzzer(total=1000):
 #     # invalid control variable causes infinite loop
 #     run_fuzzer(seed=9)
 
+
 def test_mock_scfg_fuzzer_case36():
     run_fuzzer(seed=36)
+
 
 # def test_mock_scfg_fuzzer_case146():
 #     run_fuzzer(seed=146)
