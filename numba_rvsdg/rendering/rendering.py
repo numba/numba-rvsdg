b737e2fe9d9b3f9d7020074bc5e9bd19cae8519a
esc <esc@users.noreply.github.com>
2024-06-18 19:32:33 +0200

    wip
    As title

--- numba_rvsdg/rendering/rendering.py
+++ numba_rvsdg/rendering/rendering.py
@@ -352,7 +352,7 @@ class SCFGRenderer(BaseRenderer):
         self.g.view(name)
 
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 
 def render_func(func) -> None:  # type: ignore
